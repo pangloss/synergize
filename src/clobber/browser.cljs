d4e837f439d2a02c611fb8d628c14cb1a7511902
Darrick Wiebe <dw@xnlogic.com>
2013-09-29 01:46:48 -0400

    Thought of a better way to console.log

--- src/clobber/browser.cljs
+++ src/clobber/browser.cljs
@@ -4,27 +4,10 @@
 ; Enable print / println / pr / prn in the browser.
 (set! *print-fn* #(js/console.log %))
 
-(set! pr (fn
-           ([                   ] (js/console.log                    ))
-           ([a                  ] (js/console.log a                  ))
-           ([a b                ] (js/console.log a b                ))
-           ([a b c              ] (js/console.log a b c              ))
-           ([a b c d            ] (js/console.log a b c d            ))
-           ([a b c d e          ] (js/console.log a b c d e          ))
-           ([a b c d e f        ] (js/console.log a b c d e f        ))
-           ([a b c d e f g      ] (js/console.log a b c d e f g      ))
-           ([a b c d e f g h    ] (js/console.log a b c d e f g h    ))
-           ([a b c d e f g h i  ] (js/console.log a b c d e f g h i  ))
-           ([a b c d e f g h i j] (js/console.log a b c d e f g h i j))
-           ([k l m n o p q r s t a                  ] (js/console.log k l m n o p q r s t a                  ))
-           ([k l m n o p q r s t a b                ] (js/console.log k l m n o p q r s t a b                ))
-           ([k l m n o p q r s t a b c              ] (js/console.log k l m n o p q r s t a b c              ))
-           ([k l m n o p q r s t a b c d            ] (js/console.log k l m n o p q r s t a b c d            ))
-           ([k l m n o p q r s t a b c d e          ] (js/console.log k l m n o p q r s t a b c d e          ))
-           ([k l m n o p q r s t a b c d e f        ] (js/console.log k l m n o p q r s t a b c d e f        ))
-           ([k l m n o p q r s t a b c d e f g      ] (js/console.log k l m n o p q r s t a b c d e f g      ))
-           ([k l m n o p q r s t a b c d e f g h    ] (js/console.log k l m n o p q r s t a b c d e f g h    ))
-           ([k l m n o p q r s t a b c d e f g h i  ] (js/console.log k l m n o p q r s t a b c d e f g h i  ))
-           ([k l m n o p q r s t a b c d e f g h i j] (js/console.log k l m n o p q r s t a b c d e f g h i j))))
-(set! prn pr)
+(defn pr* [& args]
+  (let [c js/console]
+    (.apply (.-log c) c (into-array args))))
+
+(set! pr pr*)
+(set! prn pr*)
 
